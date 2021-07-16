24ddac4bd83a02d07e99931805cb1f272310a4f7
wlgnsld1108 <wlgnsld1108@naver.com>
2021-07-16 19:00:52 +0900

    7.16

--- js/common.js
+++ js/common.js
@@ -3,34 +3,33 @@ $(function() {
     $('a[href="#"]').click(function(e) {
         e.preventDefault();
     });
-});
-
-// 헤더 스크롤시 변경
-$(window).on('scroll', function() {
-    var sc_top = $(this).scrollTop();
-
-    $('#bg_cover').hide();
-    $('#search_wrap, .nav_menu').slideUp(200);
-
-    if(sc_top > 760) {
-        $('.header').css('background', '#fff');
-        $('.menu_bar span').css('background', '#000');
-        $('.logo img').attr('src', 'images/logo_b.png');
-        $('.search img').attr('src', 'images/search_icon_b.png');
-        $('.globe img').attr('src', 'images/globe_icon_b.png');
-        $('#search_wrap').css('background', '#fff');
-    } else {
-        $('.header').css('background', '#008944');
-        $('.menu_bar span').css('background', '#fff');
-        $('.logo img').attr('src', 'images/logo_w.png');
-        $('.search img').attr('src', 'images/search_icon_w.png');
-        $('.globe img').attr('src', 'images/globe_icon_w.png');        
-        $('#search_wrap').css('background', '#fff');
-    }
-});
-
-// 네비게이션 햄버거 메뉴 클릭시
-$(function() {
+    
+    // 헤더 스크롤시 변경
+    $(window).on('scroll', function() {
+        var sc_top = $(this).scrollTop();
+        
+        $('#bg_cover').hide();
+        $('.search_wrap, .nav_menu').slideUp(200);
+        $('.menu_bar').removeClass('active');
+        
+        if(sc_top > 760) {
+            $('.header').css('background', '#fff');
+            $('.menu_bar span').css('background', '#000');
+            $('.logo img').attr('src', 'images/logo_b.png');
+            $('.search img').attr('src', 'images/search_icon_b.png');
+            $('.globe img').attr('src', 'images/globe_icon_b.png');
+            $('.search_wrap').css('background', '#fff');
+        } else {
+            $('.header').css('background', '#008944');
+            $('.menu_bar span').css('background', '#fff');
+            $('.logo img').attr('src', 'images/logo_w.png');
+            $('.search img').attr('src', 'images/search_icon_w.png');
+            $('.globe img').attr('src', 'images/globe_icon_w.png');        
+            $('.search_wrap').css('background', '#fff');
+        }
+    });
+    
+    // 네비게이션 햄버거 메뉴 클릭시
     $('.nav_menu').hide();
     
     $('.header .nav_bar').click(function(e) {
@@ -39,35 +38,51 @@ $(function() {
         e.stopPropagation();
         $('#bg_cover').toggle();
         $('.nav_menu').slideToggle(200);
+        
+        $(document).click(function(e){
+            if (!$(e.target).is('.nav_menu, ul, li, a')) {
+                $('.menu_bar').removeClass('active');
+                $('#bg_cover').hide();
+                $('.nav_menu').slideUp(200);
+            }
+        });
+        
+        $('.header .search').click(function(){
+            $('.menu_bar').removeClass('active');
+            $('#bg_cover').show();
+            $('.nav_menu').slideUp(200);
+        });
+    }); 
+
+    // 내비게이션 하위메뉴 해상도 1023일때
+    $('.nav_menu li.top_menu').click(function() {
+        var width = $(window).width();
+
+        if(width <= 1023) {
+            $(this).siblings('.nav_menu li:nth-child(n+2)').slideToggle(200);
+        }
     });
 
-    $(document).click(function(){
-        $('#bg_cover').hide();
-        $('.nav_menu').slideUp(200);
-    });
-    
-    $('.header .search').click(function(){
-        $('#bg_cover').hide();
-        $('.nav_menu').slideUp(200);
-    });
-});
-
-// 검색바
-$(function() {
-    $('#search_wrap').hide();
+    // 검색바
+    $('.search_wrap').hide();
     
     $('.header .search').click(function(e) {
         e.stopPropagation();
         $('#bg_cover').toggle();
-        $('#search_wrap').slideToggle(200);
+        $('.search_wrap').slideToggle(200);        
+        
+        $(document).click(function(e){
+            if (!$(e.target).is('.search_wrap, input')) {
+                $('#bg_cover').hide();
+                $('.search_wrap').slideUp(200);     
+            } 
+        });
+        
+        $('.header .nav_bar').click(function(){
+            $('#bg_cover').show();
+            $('.search_wrap').slideUp(200);
+        });
     });
-    
-    $(document).click(function(){
-        $('#bg_cover').hide();
-        $('#search_wrap').slideUp(200);
-    });
-    
-});
 
 // 패밀리사이트
 function onFamilySite(url) {
@@ -75,3 +90,5 @@ function onFamilySite(url) {
         window.open(url);
     }
 }
+
+});
