c3ad71238bc79527590b11cc57a1897366c8f3a8
wlgnsld1108 <wlgnsld1108@naver.com>
2021-07-23 19:01:06 +0900

    7.23

--- js/sub3.js
+++ js/sub3.js
@@ -1,4 +1,10 @@
-// 제품 분류 탭
+// 파일 첨부 줄 추가 및 제거
 $(function() {
-    $()
+    $('.fileplus').click(function() {
+        $('#filebox_wrap').clone().appendTo('#file_wrap').css('marginTop', '10px');
+
+            $('.fileminus').click(function() {
+                $('#filebox_wrap').next().remove();
+            });
+    });
 });
