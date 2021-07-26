a825d6ae21cc24d545fb0106b2b3cfe6f008169d
wlgnsld1108 <wlgnsld1108@naver.com>
2021-07-26 19:01:19 +0900

    07.26

--- js/sub2.js
+++ js/sub2.js
@@ -1,4 +1,10 @@
 // 제품 분류 탭
-$(function() {
-    $()
-});
+jQuery(function($){
+	$(".product_tap > ul > li > a").on("mouseover focus", function(){
+    $(this).parent().addClass("tap_on").siblings().removeClass('tap_on');
+	});
+    $('.product_tap > ul > li > a:last-child').unbind('mouseover focus').css({
+        'background': '#e9e9e9',
+        'color': '#000'
+    });
+}); 
--- js/sub3.js
+++ js/sub3.js
@@ -1,32 +1,43 @@
-// 파일 첨부 줄 추가 및 제거
-var count = 0;
-$(document).on('click', '.fileplus', function() {
-    var len = $('.filebox').length;
+// // 파일 첨부 줄 추가 및 제거
+// var count = 0;
+// $(document).on('click', '.fileplus', function() {
+//     var len = $('.filebox').length;
     
-    if(len < 3){
-        var clone = $(this).parent().clone();
-        clone.find('input').val('');
-        $('#filebox_wrap').append(clone);
-    } else {
-        alert('최대 3개까지 첨부 가능합니다.');
-    }
+//     if(len < 3){
+//         var clone = $(this).parent().clone();
+//         clone.find('input').val('');
+//         $('#filebox_wrap').append(clone);
+//     } else {
+//         alert('최대 3개까지 첨부 가능합니다.');
+//     }
+// });
+
+// $(document).on('click', '.fileminus', function(){
+//     var len = $('.filebox').length;
+//     if(len > 1){
+//         $(this).parent().remove();
+//     } else {
+//         alert('최소 1개 이상 첨부해야합니다.');
+//     }
+// });
+
+$(document).on("click", ".fileplus", function() {
+    $('#filebox_wrap').append(
+        $('<input/>').attr('type', 'file').attr('name', 'someName')
+    ).append(
+        $('<input/>').attr('type', 'button').attr({
+            value: '+',
+            class: 'fileplus'
+        })
+    );
 });
 
-$(document).on('click', '.fileminus', function(){
-    var len = $('.filebox').length;
-    if(len > 1){
-        $(this).parent().remove();
-    } else {
-        alert('최소 1개 이상 첨부해야합니다.');
-    }
-});
-
-// 파일명 추가
-$('.upload-hidden').on('change', function(){
-    if(window.FileReader){
-        var filename = $(this)[0].files[0].name;
-    } else {
-        var filename = $(this).val().split('/').pop().split('\\').pop();
-    }
-    $(this).siblings('.upload-name').val(filename);
-});
+// // 파일명 추가
+// $('.upload-hidden').on('change', function(){
+//     if(window.FileReader){
+//         var filename = $(this)[0].files[0].name;
+//     } else {
+//         var filename = $(this).val().split('/').pop().split('\\').pop();
+//     }
+//     $(this).siblings('.upload-name').val(filename);
+// });
