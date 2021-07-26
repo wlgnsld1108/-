3ff06ceba10cb4a1d6d8e6d686b44fc7ea1c0474
wlgnsld1108 <wlgnsld1108@naver.com>
2021-07-26 17:09:17 +0900

    7.26

--- js/sub3.js
+++ js/sub3.js
@@ -1,10 +1,32 @@
 // 파일 첨부 줄 추가 및 제거
-$(function() {
-    $('.fileplus').click(function() {
-        $('#filebox_wrap').clone().appendTo('#file_wrap').css('marginTop', '10px');
+var count = 0;
+$(document).on('click', '.fileplus', function() {
+    var len = $('.filebox').length;
+    
+    if(len < 3){
+        var clone = $(this).parent().clone();
+        clone.find('input').val('');
+        $('#filebox_wrap').append(clone);
+    } else {
+        alert('최대 3개까지 첨부 가능합니다.');
+    }
+});
 
-            $('.fileminus').click(function() {
-                $('#filebox_wrap').next().remove();
-            });
-    });
+$(document).on('click', '.fileminus', function(){
+    var len = $('.filebox').length;
+    if(len > 1){
+        $(this).parent().remove();
+    } else {
+        alert('최소 1개 이상 첨부해야합니다.');
+    }
+});
+
+// 파일명 추가
+$('.upload-hidden').on('change', function(){
+    if(window.FileReader){
+        var filename = $(this)[0].files[0].name;
+    } else {
+        var filename = $(this).val().split('/').pop().split('\\').pop();
+    }
+    $(this).siblings('.upload-name').val(filename);
 });
